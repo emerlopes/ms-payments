c5865f0216d3ab545124d2a34884dfc6c0430fe6
Emerson <lopes.ds@live.com>
2024-01-28 13:26:23 -0300

    refact() removendo import

--- src/main/java/com/techchallenge/mspayments/MsPaymentsApplication.java
+++ src/main/java/com/techchallenge/mspayments/MsPaymentsApplication.java
@@ -2,6 +2,7 @@ package com.techchallenge.mspayments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class MsPaymentsApplication {
