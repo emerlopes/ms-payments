e443638b424b0d967d77df9f7c4f75a35cd5bddf
Emerson <lopes.ds@live.com>
2024-01-28 13:32:31 -0300

    chore() adicionando JsonProperty

--- src/main/java/com/techchallenge/mspayments/application/entrypoint/rest/paymentreceipt/dto/PaymentReceiptDTO.java
+++ src/main/java/com/techchallenge/mspayments/application/entrypoint/rest/paymentreceipt/dto/PaymentReceiptDTO.java
@@ -1,5 +1,6 @@
 package com.techchallenge.mspayments.application.entrypoint.rest.paymentreceipt.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 import java.math.BigDecimal;
@@ -7,9 +8,18 @@ import java.time.LocalDateTime;
 
 @Getter
 public class PaymentReceiptDTO {
+    @JsonProperty("payment_date")
     private LocalDateTime paymentDate;
+
+    @JsonProperty("customer_name")
     private String customerName;
+
+    @JsonProperty("vehicle_plate_number")
     private String vehiclePlateNumber;
+
+    @JsonProperty("payment_amount")
     private BigDecimal paymentAmount;
+
+    @JsonProperty("payment_method")
     private String paymentMethod;
 }
--- src/main/java/com/techchallenge/mspayments/domain/entity/ParkingPaymentReceiptDomainEntityOutput.java
+++ src/main/java/com/techchallenge/mspayments/domain/entity/ParkingPaymentReceiptDomainEntityOutput.java
@@ -1,5 +1,6 @@
 package com.techchallenge.mspayments.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,10 +10,22 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class ParkingPaymentReceiptDomainEntityOutput {
+
+    @JsonProperty("receipt_number")
     private Long receiptNumber;
+
+    @JsonProperty("payment_date")
     private LocalDateTime paymentDate;
+
+    @JsonProperty("customer_name")
     private String customerName;
+
+    @JsonProperty("vehicle_plate_number")
     private String vehiclePlateNumber;
+
+    @JsonProperty("payment_amount")
     private BigDecimal paymentAmount;
+
+    @JsonProperty("payment_method")
     private String paymentMethod;
 }
