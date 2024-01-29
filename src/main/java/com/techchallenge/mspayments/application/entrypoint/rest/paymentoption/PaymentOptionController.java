b402643a9443f7e2a864b9065aafb025ca25195b
Emerson <lopes.ds@live.com>
2024-01-29 01:36:56 -0300

    chore() adiconando documentacao

--- src/main/java/com/techchallenge/mspayments/application/entrypoint/rest/paymentoption/PaymentOptionController.java
+++ src/main/java/com/techchallenge/mspayments/application/entrypoint/rest/paymentoption/PaymentOptionController.java
@@ -3,19 +3,28 @@ package com.techchallenge.mspayments.application.entrypoint.rest.paymentoption;
 import com.techchallenge.mspayments.application.entrypoint.rest.paymentoption.dto.PaymentOptionDTO;
 import com.techchallenge.mspayments.application.mapper.PaymentOptionMappers;
 import com.techchallenge.mspayments.application.mapper.PaymentOptionTypeMappers;
+import com.techchallenge.mspayments.domain.entity.PaymentOptionDomainEntityOutput;
+import com.techchallenge.mspayments.domain.shared.CustomData;
 import com.techchallenge.mspayments.domain.usecase.IExecuteFindPaymentOptionByExternalDriverIdUseCase;
 import com.techchallenge.mspayments.domain.usecase.IExecuteSavePaymentOptionUseCase;
 import com.techchallenge.mspayments.domain.usecase.IExecuteFindPaymentOptionsUseCase;
 import com.techchallenge.mspayments.domain.usecase.paymentoptiostype.IExecuteFindPaymentOptionTypeByIdUseCase;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/payment-options")
+@Tag(name = "Payment Options", description = "API de Opções de Pagamento")
 public class PaymentOptionController {
 
     private final IExecuteSavePaymentOptionUseCase executeCreatePaymentOptionUseCase;
@@ -30,8 +39,15 @@ public class PaymentOptionController {
         this.iExecuteFindPaymentOptionByExternalDriverIdUseCase = iExecuteFindPaymentOptionByExternalDriverIdUseCase;
     }
 
-    @RequestMapping
-    public ResponseEntity<?> savePaymentOption(@Valid @RequestBody PaymentOptionDTO paymentOptionDTO) {
+    @Operation(
+            summary = "Salvar uma nova opção de pagamento",
+            description = "Salva uma nova opção de pagamento com os dados fornecidos em PaymentOptionDTO. O campo 'paymentOptionTypeId' deve ser preenchido com uma das opções disponíveis: 1 (CREDIT_CARD), 2 (DEBIT_CARD) ou 3 (PIX)."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Operação bem-sucedida, nova opção de pagamento criada e retornada.")
+    })
+    @PostMapping
+    public ResponseEntity<CustomData<PaymentOptionDomainEntityOutput>> savePaymentOption(@Valid @RequestBody PaymentOptionDTO paymentOptionDTO) {
         final var paymentOptionDoamainEntityInput = PaymentOptionMappers.mapToPaymentOptionDomainEntityInput(paymentOptionDTO);
         final var paymentOptionType = executeFindPaymentOptionTypeByIdUseCase.execute(paymentOptionDTO.getPaymentOptionTypeId());
         final var paymentOptionTypeEntity = PaymentOptionTypeMappers.mapToPaymentOptionEntity(paymentOptionType.getData());
@@ -42,14 +58,24 @@ public class PaymentOptionController {
         return ResponseEntity.status(HttpStatus.CREATED).body(paymentOption);
     }
 
+    @Operation(summary = "Buscar todas as opções de pagamento", description = "Retorna todas as opções de pagamento disponíveis."
+    )
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "200", description = "Operação bem-sucedida, lista de opções de pagamento retornada.")
+    })
     @GetMapping
-    public ResponseEntity<?> findPaymentOptions() {
+    public ResponseEntity<CustomData<List<PaymentOptionDomainEntityOutput>>> findPaymentOptions() {
         final var paymentOptions = executeFindPaymentOptionsUseCase.execute();
         return ResponseEntity.status(HttpStatus.OK).body(paymentOptions);
     }
 
+    @Operation(summary = "Buscar opção de pagamento por ID externo", description = "Retorna uma opção de pagamento específica por condutor com base em seu ID externo."
+    )
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "200", description = "Operação bem-sucedida, opção de pagamento encontrada e retornada.")
+    })
     @GetMapping("/{externalDriverId}")
-    public ResponseEntity<?> findPaymentOptionByExternalDriverId(@PathVariable UUID externalDriverId) {
+    public ResponseEntity<CustomData<PaymentOptionDomainEntityOutput>> findPaymentOptionByExternalDriverId(@PathVariable UUID externalDriverId) {
         final var paymentOption = iExecuteFindPaymentOptionByExternalDriverIdUseCase.execute(externalDriverId);
         return ResponseEntity.status(HttpStatus.OK).body(paymentOption);
     }
--- src/main/java/com/techchallenge/mspayments/application/entrypoint/rest/paymentreceipt/PaymentReceiptController.java
+++ src/main/java/com/techchallenge/mspayments/application/entrypoint/rest/paymentreceipt/PaymentReceiptController.java
@@ -2,7 +2,13 @@ package com.techchallenge.mspayments.application.entrypoint.rest.paymentreceipt;
 
 import com.techchallenge.mspayments.application.entrypoint.rest.paymentreceipt.dto.PaymentReceiptDTO;
 import com.techchallenge.mspayments.application.mapper.ParkingPaymentReceiptMappers;
+import com.techchallenge.mspayments.domain.entity.ParkingPaymentReceiptDomainEntityOutput;
+import com.techchallenge.mspayments.domain.shared.CustomData;
 import com.techchallenge.mspayments.domain.usecase.parkingpaymenteeceipt.ExecuteIssuanceReceiptPaymentUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/payment-receipts")
+@Tag(name = "Payment Receipts", description = "API de Comprovantes de Pagamento")
 public class PaymentReceiptController {
 
     private final ExecuteIssuanceReceiptPaymentUseCase executeIssuanceReceiptPaymentUseCase;
@@ -20,8 +27,15 @@ public class PaymentReceiptController {
         this.executeIssuanceReceiptPaymentUseCase = executeIssuanceReceiptPaymentUseCase;
     }
 
+    @Operation(
+            summary = "Salvar um novo comprovante de pagamento",
+            description = "Salva um novo comprovante de pagamento com os dados fornecidos em PaymentReceiptDTO."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Operação bem-sucedida, novo comprovante de pagamento criado e retornado.")
+    })
     @PostMapping
-    public ResponseEntity<?> savePaymentReceipt(@RequestBody PaymentReceiptDTO paymentReceiptDTO) {
+    public ResponseEntity<CustomData<ParkingPaymentReceiptDomainEntityOutput>> savePaymentReceipt(@RequestBody PaymentReceiptDTO paymentReceiptDTO) {
         final var input = ParkingPaymentReceiptMappers.mapToParkingPaymentReceiptDomainEntityInput(paymentReceiptDTO);
         final var result = executeIssuanceReceiptPaymentUseCase.execute(input);
 
